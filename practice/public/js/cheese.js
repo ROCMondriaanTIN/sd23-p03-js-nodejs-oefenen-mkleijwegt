c13f90a12a4ff082933a63e991ba876eb735118b
Mark Kleijwegt <mark.kleijwegt@gmail.com>
2024-03-24 21:12:12 +0100

    implemented cheese with bootstrap

--- practice/public/js/cheese.js
+++ practice/public/js/cheese.js
@@ -3,6 +3,7 @@ console.log('Cheese loaded');
 //select container from html
 const container = document.querySelector('.container');
 
+
 //fetching cheeses from node js server
 fetch('/cheeses')
     .then(myData => myData.json())
@@ -12,13 +13,14 @@ fetch('/cheeses')
 function showCards(cheeses) {
     console.log(cheeses);
 
-    let htmlCode = '';
+    let htmlCode = '<div class="row">';
     //for loop that loops over the cheese array and generates a card for each cheese
     for (let i = 0; i < cheeses.length; i++) {
         const cheese = cheeses[i];
         //calling the create card function that returns the html for a card
         htmlCode += createCard(cheese);
     }
+    htmlCode += '</div>';
     //adding the html to the webpage
     container.innerHTML = htmlCode;
 }
@@ -27,10 +29,12 @@ function showCards(cheeses) {
 function createCard(cheese) {
     //we use the back tick here to ease our minds adding in variables (no plusjes yay!)
     const card = `
-        <div class="card">
-            <h2>${cheese.name}</h2>
-            <img src="${cheese.image}" />
-            <p>${cheese.description}</p>
+        <div class="col-md-4 d-flex align-items-stretch">
+            <div class="card">
+                <h2>${cheese.name}</h2>
+                <img class="card-img-top" src="${cheese.image}" />
+                <p>${cheese.description}</p>
+            </div>
         </div>
     `;
     return card;
