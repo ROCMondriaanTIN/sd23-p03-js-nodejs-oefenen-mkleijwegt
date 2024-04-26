c2c5dd3557dd2f6e83ec2adf4ce1b841a9039ff6
Mark Kleijwegt <mark.kleijwegt@gmail.com>
2024-04-26 10:32:17 +0200

    pizza practice

---
+++ pizza/index.js
@@ -0,0 +1,80 @@
+import express from 'express';
+import { } from 'dotenv/config';
+import { MongoClient } from 'mongodb';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+const url = process.env.CONNECTION_URL;
+console.log(url);
+
+const client = new MongoClient(url);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/pizzas', (req, res) => {
+    fetchPizza().then(pizzas => {
+        res.json(pizzas);
+    });
+});
+
+app.post('/add-pizza', (req, res) => {
+    const name = req.body.name;
+    const description = req.body.description;
+    const price = req.body.price;
+
+
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
+async function fetchPizza() {
+    try {
+        await client.connect();
+
+        // Get the database and collection on which to run the operation
+        const db = client.db('test');
+        const col = db.collection('pizza');
+
+        // Find the document
+        const documents = await col.find().toArray();
+
+        return documents;
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+
+async function insertPizza(name, description, price) {
+
+    try {
+        // Connect to the Atlas cluster
+        await client.connect();
+
+        // Get the database and collection on which to run the operation
+        const db = client.db("test");
+        const col = db.collection("pizza");
+
+        // Insert the documents into the specified collection        
+        const p = await col.insertOne({ name: name, description: description, price: price });
+        // Print results
+
+        console.log('Pizza toegevoegd');
+
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
