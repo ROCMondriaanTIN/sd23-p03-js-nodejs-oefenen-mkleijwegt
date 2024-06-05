ae8701fdfd94e52a2c54e2387ede457215f0e1a5
Mark Kleijwegt <mark.kleijwegt@gmail.com>
2024-06-05 15:30:29 +0200

    setup chart with crypto api

--- olc-oefen/public/js/main.js
+++ olc-oefen/public/js/main.js
@@ -3,12 +3,78 @@ console.log('Main loaded');
 const cardContainer = document.querySelector('.card-container');
 const chart1 = document.querySelector('.chart-1');
 const chart2 = document.querySelector('.chart-2');
+const searchInput = document.querySelector('.search-input');
+const searchButton = document.querySelector('.search-button');
 
-fetch('/games')
+searchButton.addEventListener('click', function(){
+
+    const searchValue = searchInput.value;
+
+    fetch(`https://api.coincap.io/v2/assets/${searchValue}/history?interval=d1`)
+        .then(data => data.json())
+        .then(jsonData => showDataByTime(jsonData));
+});
+
+fetch('https://api.coincap.io/v2/assets')
     .then(data => data.json())
     .then(jsonData => showData(jsonData));
 
-function showData(jsonData){
+
+
+function showDataByTime(cryptoDateTimeData){
+    const labels = [];
+    const data = [];
+    for (let i = 0; i < cryptoDateTimeData.data.length; i++) {
+        const crypto = cryptoDateTimeData.data[i];
+        labels.push(crypto.date);
+        data.push(crypto.priceUsd);
+    }
+    console.log(labels);
+    console.log(data);
+    createChart(chart2, labels, data, 'Crypto by date', 'line');
+}
+
+
+function showData(cryptos){
+    console.log(cryptos);
+    const labels = [];
+    const data = [];
+    for (let i = 0; i < cryptos.data.length; i++) {
+        const crypto = cryptos.data[i];
+        if(crypto.id === 'bitcoin' || crypto.id === 'ethereum'){
+            labels.push(crypto.id);
+            data.push(crypto.priceUsd);
+        }
+    }
+    console.log(labels);
+    console.log(data);
+    createChart(chart1, labels, data, 'Crypto', 'bar');
+
+}
+function createChart(canvas, labels, data, label, type){
+    new Chart(canvas, {
+        type: type,
+        data: {
+        //de labels komen op de x as
+        labels: labels,
+        datasets: [{
+            label: label,
+            // de data komt op de y as
+            data: data,
+            borderWidth: 1
+        }]
+        },
+        options: {
+        scales: {
+            y: {
+            beginAtZero: true
+            }
+        }
+        }
+    });
+}
+
+/*function showData(jsonData){
     showCards(jsonData);
     showCharts(jsonData);
 }
@@ -51,4 +117,4 @@ function createCard(game){
     </div>
     `;
     return card;
-}
+}*/
